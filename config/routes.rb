782e28258b002b4577e07e44743bd6bc9606db6f
Ali Waqar <aliwaqar981@gmail.com>
2019-02-27 10:04:21 +0500

    Added new articles by making a form and validate title and description

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   root  to:'pages#home'
-
+  get 'articles/index', to:'articles#index'
   get 'about', to:'pages#about' 
+
+  resources :articles
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
