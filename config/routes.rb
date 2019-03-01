57ee62c14558d1e5b95ec80311974ec57d6db097
Ali Waqar <aliwaqar981@gmail.com>
2019-03-01 19:12:01 +0500

    Authenticate user from UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
 
   root  to:'pages#home'
   get 'articles/index', to:'articles#index'
-  get 'about', to:'pages#about' 
+  get 'about', to:'pages#about'
 
   resources :articles
+
+  get 'signup', to:'users#new'
+  resources :users, except: [:new]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
