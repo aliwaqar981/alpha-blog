1bf550e69cf94a8cb49d6997b6fe1cdebc8dade3
Ali Waqar <aliwaqar981@gmail.com>
2019-03-05 16:33:12 +0500

    Completed Authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
 
     def create
        @article=Article.new(article_params)
-       @article.user=User.find(4)
+       @article.user=current_user
        if @article.save
             flash[:success]="Article was created successfully"
             redirect_to article_path(@article)
