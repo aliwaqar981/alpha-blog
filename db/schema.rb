7f2e18a627f6188588c0a3080ed15d23047d7b3d
Ali Waqar <aliwaqar981@gmail.com>
2019-03-06 11:31:01 +0500

    Added admin user and defined access for admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_114958) do
+ActiveRecord::Schema.define(version: 2019_03_05_133128) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_03_01_114958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
